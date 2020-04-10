a8cb0165ccb85929b8b0213ea5df4963e76f7a9c
dev_sl <you@example.com>
2020-04-10 16:03:40 +0800

    add ignore.bat delete surplus program file

---
+++ Development/ignore.bat
@@ -0,0 +1,34 @@
+cd Samples\VC
+for /d %%i in (*) do ( 
+cd %%i
+rd x86 x64 /s /q
+del *.sdf *.suo *.log *.user *.filters *.ipch *.aps /s
+cd..)
+cd..
+cd QT
+for /d %%i in (*) do ( 
+cd %%i
+rd x86 x64 /s /q
+del *.sdf *.suo *.log *.user *.filters *.ipch *.aps /s
+cd..)
+cd..
+cd OpenCV\VC\KSJDemoVC_Opencv
+rd x86 x64 /s /q
+del *.sdf *.suo *.log *.user *.filters *.ipch *.aps /s
+cd.. 
+cd.. 
+cd..
+cd Halcon\VC\KSJDemoVC_Halcon
+rd x86 x64 /s /q
+del *.sdf *.suo *.log *.user *.filters *.ipch *.aps /s
+cd.. 
+cd..
+cd C#\KSJDemoCSharp_Halcon
+rd obj /s /q
+del *.suo /s
+cd.. 
+cd.. 
+cd..
+cd C#\KSJDemoCSharp_Callback
+rd obj /s /q
+del *.suo /s
