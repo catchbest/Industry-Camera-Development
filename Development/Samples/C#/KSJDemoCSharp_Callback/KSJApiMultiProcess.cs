2c8405e5b29e58ab801952796403ac67bce76177
Linbin.Wei <2881377368@qq.com>
2020-12-11 15:21:34 +0800

    线阵相机固定帧率寄存器最小值改为2（最大帧率50000）

--- Development/Samples/C#/KSJDemoCSharp_Callback/KSJApiMultiProcess.cs
+++ Development/Samples/C#/KSJDemoCSharp_Callback/KSJApiMultiProcess.cs
@@ -8,12 +8,9 @@ namespace KSJApi_MultiProcess
 {
     class KSJApiMultiProcess
     {
-                // 参考“凯视佳工业相机内部API函数说明书（多进程控制相机篇）”
+        // 参考“凯视佳工业相机内部API函数说明书（多进程控制相机篇）”
         [DllImport("KSJApi.dll")]
-        public static extern int KSJ_Open(int nChannel);
-        [DllImport("KSJApi.dll")]
-        public static extern int KSJ_Close(int nChannel);
-
+        public static extern int KSJ_DeviceOpen(int nChannel);
         // 关闭指定索引的设备，注意所有第一个参数为nChannle的函数都会自动打开指定的相机，如果不关闭指定相机，那么在多进程下是不能操作这台相机的。
         [DllImport("KSJApi.dll")]
         public static extern int KSJ_DeviceClose(int nChannel);
